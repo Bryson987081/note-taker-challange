b117b17d3ec2c5860730c59723c55cdc03ba2ffe
Bryson <brysob7617@gmail.com>
2024-07-18 22:41:12 -0400

    added code to notes.js file to retrieve the notes and post the new notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,32 @@
-const notes = require('express').Router();
+const note = require('express').Router();
 const uuid = require('../helpers/uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+note.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.join(JSON.parse(data)))
+);
+
+note.post('/', (req, res) => {
+    const { title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.join(response);
+    } else {
+        res.join('Error postin note');
+    }
+});
+
+module.exports = note;
